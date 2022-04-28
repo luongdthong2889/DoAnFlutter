4e09acd5a900a9ced3f51d4f1820f782ee805360
luongdthong2889 <luong.dthong@gmail.com>
2022-04-28 17:13:57 +0700

    bug start_workout RangeError
    bug start_workout RangeError

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# DoAnFlutter
+# Ứng dụng tập Yoga , Gym - Nhóm 10
+## Danh sách thành viên
+* Lương Đình Thông: 18DH110831
+* Dương Quốc Huy: 18DH110826
+* Lâm Trường Phát: 18DH110839
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ fitness_flutter/lib/screens/workout_details_screen/widget/panel/workout_details_panel.dart
@@ -0,0 +1,95 @@
+import 'package:fitness_flutter/core/const/path_constants.dart';
+import 'package:fitness_flutter/core/const/text_constants.dart';
+import 'package:fitness_flutter/data/workout_data.dart';
+import 'package:fitness_flutter/screens/workout_details_screen/widget/panel/exercises_list.dart';
+import 'package:fitness_flutter/screens/workout_details_screen/widget/panel/workout_tag.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutDetailsPanel extends StatelessWidget {
+  final WorkoutData workout;
+
+  WorkoutDetailsPanel({required this.workout});
+
+  @override
+  Widget build(BuildContext context) {
+    return _createPanelData(context);
+  }
+
+  Widget _createPanelData(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 15),
+        _createRectangle(),
+        const SizedBox(height: 15),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _createHeader(),
+              const SizedBox(height: 20),
+              _createWorkoutData(context),
+              SizedBox(height: 20),
+              _createExerciesList(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _createRectangle() {
+    return Image(image: AssetImage(PathConstants.rectangle));
+  }
+
+  Widget _createHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(
+        workout.title! + "  " + TextConstants.workout,
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _createWorkoutData(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          WorkoutTag(
+            icon: PathConstants.timeTracker,
+            content: "${_getExerciseTime()}:00",
+          ),
+          const SizedBox(width: 15),
+          WorkoutTag(
+            icon: PathConstants.exerciseTracker,
+            content:
+                '${workout.exerciseDataList!.length} ${TextConstants.exercisesLowercase}',
+          ),
+        ],
+      ),
+    );
+  }
+
+  int _getExerciseTime() {
+    int time = 0;
+    final List<int?> exerciseList =
+        workout.exerciseDataList!.map((e) => e.minutes).toList();
+    exerciseList.forEach((e) {
+      time += e!;
+    });
+    return time;
+  }
+
+  Widget _createExerciesList() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ExercisesList(exercises: workout.exerciseDataList!, workout: workout), //fix bug
+      ),
+    );
+  }
+}
